de80b7f4c9b131e1dafc6d0c79a0e66f0601a836
danielbukhosi <danieltshuma64@gmail.com>
2026-03-14 20:24:12 +0100

    fixed query logic in query5.sql

--- business_queries/sql/query5.sql
+++ business_queries/sql/query5.sql
@@ -5,7 +5,7 @@
 select count(a.actor_id) as number_of_appearances,
        concat(a.first_name,' ',a.last_name) as actor_full_name,
        c.name as category_name,
-	   dense_rank() over( partition by c.name order by count(a.actor_id) desc ) as popularity_rank
+	   dense_rank() over(order by count(a.actor_id) desc ) as popularity_rank
 from actor a
 left join film_actor fa
 on a.actor_id = fa.actor_id
@@ -15,6 +15,5 @@ left join film_category fc
 on f.film_id = fc.film_id
 left join category c
 on fc.category_id = c.category_id
+where c.name = 'Children'
 group by actor_full_name, category_name
-having name = 'Children'
-limit 6
