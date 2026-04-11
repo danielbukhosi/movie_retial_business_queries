6ad189e55f273874195a3052d4377347ed0ec55e
danielbukhosi <danieltshuma64@gmail.com>
2026-04-11 21:22:24 +0200

    added UNION ALL to separate statements

--- business_queries/sql/query7.sql
+++ business_queries/sql/query7.sql
@@ -3,7 +3,7 @@
  -- Do the same for cities that have a “-” in them. Write everything in one query.
 */
 
-select 
+(select 
        distinct ca.name as category_name,
 	   ci.city,
 	   sum(f.rental_duration) over(partition by city) as total_rental_duration
@@ -22,6 +22,34 @@ left join address a
 on c.address_id = a.address_id
 inner join city ci
 on a.city_id = ci.city_id
-where city like '%-%' and ca.name ilike 'A%'  -- using wildcards
+where ca.name ilike 'A%'  -- using wildcards
 order by total_rental_duration desc
+limit 1)
+
+UNION ALL
+
+(select 
+       distinct ca.name as category_name,
+	   ci.city,
+	   sum(f.rental_duration) over(partition by city) as total_rental_duration
+from category ca
+left join film_category fc
+on ca.category_id = fc.category_id
+left join film f
+on fc.film_id = f.film_id
+left join inventory i
+on f.film_id = i.film_id
+left join rental r
+on i.inventory_id = r.inventory_id
+left join customer c
+on r.customer_id = c.customer_id
+left join address a
+on c.address_id = a.address_id
+inner join city ci
+on a.city_id = ci.city_id
+where city like '%-%'   -- using wildcards
+order by total_rental_duration desc
+limit 1);
+
+
 
