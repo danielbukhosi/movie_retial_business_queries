09d957428a5f2919d0d8e095eb441ddb9d4a9832
danielbukhosi <danieltshuma64@gmail.com>
2026-04-12 20:01:26 +0200

    fixed query error in query7.sql

--- business_queries/sql/query7.sql
+++ business_queries/sql/query7.sql
@@ -3,53 +3,59 @@
  -- Do the same for cities that have a “-” in them. Write everything in one query.
 */
 
-(select 
-       distinct ca.name as category_name,
-	   ci.city,
-	   sum(f.rental_duration) over(partition by city) as total_rental_duration
-from category ca
-left join film_category fc
-on ca.category_id = fc.category_id
-left join film f
-on fc.film_id = f.film_id
-left join inventory i
-on f.film_id = i.film_id
-left join rental r
-on i.inventory_id = r.inventory_id
-left join customer c
-on r.customer_id = c.customer_id
-left join address a
-on c.address_id = a.address_id
-inner join city ci
-on a.city_id = ci.city_id
-where ca.name ilike 'A%'  -- using wildcards
-order by total_rental_duration desc
-limit 1)
 
-UNION ALL
+WITH CategoryRankings_01 AS (
+    SELECT 
+        ca.name AS category_name,
+        ci.city,
+        SUM(f.rental_duration) AS total_rental_hours, 
+        DENSE_RANK() OVER(PARTITION BY ci.city ORDER BY SUM(f.rental_duration) DESC) AS total_rental_hours_ranking
+    FROM category ca
+    LEFT JOIN film_category fc ON ca.category_id = fc.category_id
+    LEFT JOIN film f ON fc.film_id = f.film_id
+    LEFT JOIN inventory i ON f.film_id = i.film_id
+    LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+    LEFT JOIN customer c ON r.customer_id = c.customer_id
+    LEFT JOIN address a ON c.address_id = a.address_id
+    INNER JOIN city ci ON a.city_id = ci.city_id
+    WHERE ca.name ILIKE 'A%'
+    GROUP BY ca.name, ci.city
+	ORDER BY total_rental_hours DESC
+),
 
-(select 
-       distinct ca.name as category_name,
-	   ci.city,
-	   sum(f.rental_duration) over(partition by city) as total_rental_duration
-from category ca
-left join film_category fc
-on ca.category_id = fc.category_id
-left join film f
-on fc.film_id = f.film_id
-left join inventory i
-on f.film_id = i.film_id
-left join rental r
-on i.inventory_id = r.inventory_id
-left join customer c
-on r.customer_id = c.customer_id
-left join address a
-on c.address_id = a.address_id
-inner join city ci
-on a.city_id = ci.city_id
-where city like '%-%'   -- using wildcards
-order by total_rental_duration desc
-limit 1);
+CategoryRankings_02 AS (
+    SELECT 
+        DISTINCT ca.name AS category_name,
+        ci.city,
+        SUM(f.rental_duration) AS total_rental_hours, 
+        DENSE_RANK() OVER(PARTITION BY ci.city ORDER BY SUM(f.rental_duration) DESC) AS total_rental_hours_ranking
+    FROM category ca
+    LEFT JOIN film_category fc ON ca.category_id = fc.category_id
+    LEFT JOIN film f ON fc.film_id = f.film_id
+    LEFT JOIN inventory i ON f.film_id = i.film_id
+    LEFT JOIN rental r ON i.inventory_id = r.inventory_id
+    LEFT JOIN customer c ON r.customer_id = c.customer_id
+    LEFT JOIN address a ON c.address_id = a.address_id
+    INNER JOIN city ci ON a.city_id = ci.city_id
+    WHERE ci.city LIKE '%-%'
+    GROUP BY ca.name, ci.city
+	ORDER BY total_rental_hours DESC
+),
+
+all_category_rankings  AS(
+
+  (
+	  SELECT * FROM CategoryRankings_01
+	  WHERE total_rental_hours_ranking = 1
+  )
+  UNION ALL
+  (
+	  SELECT * FROM CategoryRankings_02
+	  WHERE total_rental_hours_ranking = 1
+  )
+)
+SELECT * from all_category_rankings;
+   
 
 
 
